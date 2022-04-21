2101da9388f48bc3e60eccc3a0208396819572ee
Yousef Ashraf <bosayousef981@gmail.com>
2022-04-21 02:47:16 +0200

    Fetch Data from backend Add carousel model Add Image Grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import Carousel from './components/Carousel';
+import ImageGrid from './components/ImageGrid';
+import axios from 'axios';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [showCarousel, setShowCarousel] = useState(false);
+	const [currentImageId, setCurrentImageId] = useState(0);
+	const [images, setImages] = useState(null);
+	useEffect(() => {
+		async function fetchData() {
+			const res = await axios.get(
+				'https://scaleflex.cloudimg.io/v7/0.fe_task_static/pictures.json?vh=7a646d&func=proxy'
+			);
+			setImages(res.data);
+		}
+		fetchData();
+		console.log(images);
+	}, []);
+	return (
+		<div className="App">
+			{showCarousel && (
+				<Carousel
+					images={images}
+					currentImageId={currentImageId}
+					setCurrentImageId={setCurrentImageId}
+					setShowCarousel={setShowCarousel}
+				/>
+			)}
+			<ImageGrid
+				images={images}
+				setCurrentImageId={setCurrentImageId}
+				setShowCarousel={setShowCarousel}
+			/>
+		</div>
+	);
 }
 
 export default App;
